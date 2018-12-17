52babd10c6c8fa8ae25906085b4ca99e490d88c2
sindhu <amruta.talam@gmail.com>
2018-12-17 12:52:56 +0530

    Sindhu | imported mockito into Game2048Test class.

--- src/test/java/controller/Game2048Test.java
+++ src/test/java/controller/Game2048Test.java
@@ -4,11 +4,12 @@ import controller.Game2048;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class Game2048Test {
 
     private Game2048 game2048 = new Game2048();
-    PopUpMessage popUpMessage = mock(PopUpMessage.class);
+    private PopUpMessage popUpMessage = mock(PopUpMessage.class);
 
     @Test
     void shouldPopUpMessageYouWonWhen2048TileAppearsInGrid() {
