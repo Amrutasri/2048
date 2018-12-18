897137075bc64b4ea96ac71d57f1cdfb75da7d53
sindhu <amruta.talam@gmail.com>
2018-12-18 15:04:36 +0530

    Sindhu | covered all the test cases for Tile class by adding the behavior to it.

--- src/main/java/model/Tile.java
+++ src/main/java/model/Tile.java
@@ -1,6 +1,35 @@
 package model;
 
+import java.util.Objects;
+import java.util.Random;
+
 public class Tile {
 
-    int value;
+    private int value;
+
+    public Tile() {
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        value = randomNumberGenerator.generateEitherOfTwoNumbers2And4();
+    }
+
+    public void updateValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Tile tile = (Tile) object;
+        return value == tile.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
--- src/test/java/model/TileTest.java
+++ src/test/java/model/TileTest.java
@@ -1,5 +1,46 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TileTest {
 
+    private Tile tile1 = new Tile();
+    private Tile tile2 = new Tile();
+
+    @Test
+    void shouldEqualTheTileWithItself() {
+        assertEquals(tile1,tile1);
+    }
+
+    @Test
+    void shouldEqualAnotherWithSameValue() {
+        tile2.updateValue(tile1.getValue());
+        assertEquals(tile1,tile2);
+    }
+
+    @Test
+    void shouldNotEqualTile1WithValue2AndTile2WithValue3() {
+        tile1.updateValue(2);
+        tile2.updateValue(3);
+        assertNotEquals(tile1,tile2);
+    }
+
+    @Test
+    void shouldUpdateTheValueTo3() {
+        tile1.updateValue(3);
+        assertEquals(3, tile1.getValue());
+    }
+
+    @Test
+    void shouldReturnTheValue4WhichIsUpdated() {
+        tile1.updateValue(4);
+        assertEquals(4, tile1.getValue());
+    }
+
+    @Test
+    void shouldNotEqualTheValueWhenNotUpdated4() {
+        assertNotEquals(5,tile1.getValue());
+    }
 }
